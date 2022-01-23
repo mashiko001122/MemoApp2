a6b2764421250992b757c74c060cdf342743530c
shun <83913788+mashiko001122@users.noreply.github.com>
2022-01-23 23:53:53 +0900

    Updated memo data on MemoEditScreen

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 import { format } from 'date-fns'
 
-export function dataToString(date) {
+export function dateToString(date) {
     if (!date) { return ''; }
     return format(date, 'yyyy年M月d日 HH時mm分');
 }
